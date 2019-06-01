6a5266223752eb37843ffae52513449ff18042c8
AnimeshN <animesh.nauti@gmail.com>
2019-06-01 15:49:38 +0530

    Add files via upload

---
+++ geocode_script.R
@@ -0,0 +1,22 @@
+install.packages('devtools')
+library(devtools)
+devtools::install_github("dkahle/ggmap")
+library(ggmap)
+register_google(key = "AIzaSyCIX3eZOkPIM0PBeCsfQEt3n3MCw1MztBU")
+addresses <- read.csv("input.csv", header=TRUE, stringsAsFactors = FALSE)
+address_list <- unique(addresses$address)
+
+###CREATING A DATA FRAME TO APPEND VALUES TO ###
+address <- c()
+lat <- c()
+long <- c()
+
+###GEOCODE###
+for (i in 1:length(address_list)) {
+  coord <- geocode(address_list[i])
+  address <- c(address,address_list[i])
+  lat <- c(lat,coord[2])
+  long <- c(long,coord[1])
+}
+
+address_with_lat_long <- data.frame(address,lat,long)
